3d6ff2f637185920afe880f6057f0fe01faeae2f
Anne-Sophie SAINT-OMER <anne-sophie.saint-omer@waykonect.com>
2024-06-16 20:39:29 +0200

    SuccessScreen design

--- app/src/main/java/com/technicaltest/myyummyform/screen/SuccessScreen.kt
+++ app/src/main/java/com/technicaltest/myyummyform/screen/SuccessScreen.kt
@@ -1,30 +1,64 @@
 package com.technicaltest.myyummyform.screen
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import com.technicaltest.myyummyform.R
+import com.technicaltest.myyummyform.composable.YummyButton
 import com.technicaltest.myyummyform.navigation.Home
 
 @Composable
 fun SuccessScreen(navController: NavHostController, textFile: String) {
 
-    Column {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceAround
+    ) {
 
-        Text("Congratulations your file was successufully created!")
+        Text(
+            text = stringResource(id = R.string.success_congrats_title),
+            style = TextStyle(
+                color = colorResource(id = R.color.purple_700),
+                fontSize = 32.sp,
+                textAlign = TextAlign.Center
+            )
+        )
 
-        Text(textFile)
+        Text(
+            text = textFile
+        )
 
-        Text("You can find a file with your answers in your Documents")
+        Text(
+            modifier = Modifier.padding(horizontal = 16.dp),
+            text = stringResource(id = R.string.success_info_text),
+            style = TextStyle(
+                textAlign = TextAlign.Center,
+                color = colorResource(id = R.color.purple_500),
+                fontSize = 16.sp,
+            )
+        )
 
-        Button(onClick = {}) {
-            Text("Send by email")
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            YummyButton(R.string.success_send_by_email_button) {
+
+            }
+
+            YummyButton(R.string.success_finish_button) {
+                navController.navigate(Home)
+            }
         }
-
-        Button(onClick = { navController.navigate(Home) }) {
-            Text("Finish")
-        }
-
     }
 }
